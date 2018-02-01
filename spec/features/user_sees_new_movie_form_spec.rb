cdb0163a5ff84a17b97cf833f84031df65b462e7
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-01 16:00:21 -0700

    Adds new movie view and feature test

---
+++ spec/features/user_sees_new_movie_form_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "user sees new movie form" do
+  describe "they visit movies/new" do
+    it "shows new movie form" do
+
+      visit '/movies/new'
+
+      expect(page).to have_content("Enter Movie Info:")
+      expect(page).to have_field("movie[title]")
+      expect(page).to have_field("movie[description]")
+      expect(page).to have_selector("input[type=submit]")
+    end
+  end
+end
