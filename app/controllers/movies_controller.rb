fada967d3e7fad11a7a529a28a82573671daf784
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-05 14:39:08 -0700

    Adds nested functionality and fixes tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,25 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def new
+    @director = Director.find(params[:director_id])
+    @movie = @director.movies.new
+  end
+
+  def create
+    @director = Director.find(params[:director_id])
+    @movie = @director.movies.new(movie_params)
+    if @movie.save
+      redirect_to director_movies_path(@director)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end
 end
